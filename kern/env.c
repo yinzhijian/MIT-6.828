51ad6380c230875bb482e344430b45824ffde01e
yintao <yintao000@gmail.com>
2017-03-29 00:20:31 +0800

    lab3 exercise 2 env_setup_vm

--- kern/env.c
+++ kern/env.c
@@ -116,6 +116,14 @@ env_init(void)
 {
 	// Set up envs array
 	// LAB 3: Your code here.
+	for (int i =0;i<NENV;i++)
+	{
+		if (i < NENV-1)
+			envs[i].env_link = envs+i+1;
+		envs[i].env_id = 0;
+	}
+	env_free_list = envs;
+	
 
 	// Per-CPU part of the initialization
 	env_init_percpu();
@@ -179,6 +187,9 @@ env_setup_vm(struct Env *e)
 	//    - The functions in kern/pmap.h are handy.
 
 	// LAB 3: Your code here.
+	p->pp_ref++;
+	e->env_pgdir = page2kva(p);
+	memcpy(e->env_pgdir,kern_pgdir,PGSIZE);
 
 	// UVPT maps the env's own page table read-only.
 	// Permissions: kernel R, user R
--- kern/pmap.c
+++ kern/pmap.c
@@ -165,6 +165,8 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs  = (struct Env *) boot_alloc(sizeof(struct Env)*NENV);
+	memset(envs, 0, sizeof(struct Env)*NENV);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -199,6 +201,8 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	uint32_t env_size = ROUNDUP(NENV*sizeof(struct Env), PGSIZE);
+	boot_map_region(kern_pgdir,UENVS,env_size,PADDR(envs),PTE_W | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
