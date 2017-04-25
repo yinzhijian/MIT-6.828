febc35f074c296a40bf0bbe91f2feebbea797bbb
yintao <yintao000@gmail.com>
2017-04-25 23:16:14 +0800

    complete lab5 Exercise 1

--- kern/init.c
+++ kern/init.c
@@ -66,7 +66,7 @@ i386_init(void)
 	ENV_CREATE(user_icode, ENV_TYPE_USER);
 	//ENV_CREATE(user_primes, ENV_TYPE_USER);
 	//ENV_CREATE(user_dumbfork, ENV_TYPE_USER);
-	ENV_CREATE(user_forktree, ENV_TYPE_USER);
+	//ENV_CREATE(user_forktree, ENV_TYPE_USER);
 	//ENV_CREATE(user_yield, ENV_TYPE_USER);
 	//ENV_CREATE(user_yield, ENV_TYPE_USER);
 	//ENV_CREATE(user_yield, ENV_TYPE_USER);
